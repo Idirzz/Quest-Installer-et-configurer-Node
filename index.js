c0a89106810dd56651157e4da1fa1dcc1c6d9880
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-10-14 11:48:45 +0200

    add index.js (beautiful cow) + package.json + gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+const cowsay = require("cowsay");
+require("dotenv").config();
+
+console.log(
+  cowsay.say({
+    text: `Hello I'm ${process.env.NAME} from ${process.env.CAMPUS}!`,
+    eyes: "oo",
+  })
+);
